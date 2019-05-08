df609f7b77eaa5edbc5db521918a56908195f3a6
sumi419 <sasgar04@gmail.com>
2019-05-07 23:35:21 -0700

    💖 Update App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>Hi! I'm Sumayyah.</header>
+    <div>
+      <header>Hi! I'm Sumayyah.</header>
     </div>
   );
 }
-
-export default App;
