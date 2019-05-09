a0a6dd7fa0779f8c2af3c6cc7850619c87a7c61b
sumi419 <sasgar04@gmail.com>
2019-05-08 23:12:31 -0700

    💎 Import typeface-roboto-mono

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'typeface-roboto-mono';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
