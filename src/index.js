c0adfbf664722db86026ddfc3e855bfbf4a14b49
sumi419 <sasgar04@gmail.com>
2019-05-08 23:18:33 -0700

    🖤 Update imports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './services/serviceWorker';
 
 import 'typeface-roboto-mono';
-import './index.css';
+import './services/reset.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
