5f62cc3c6e1620df47f23660972abcd4d38ddc3a
sumi419 <sasgar04@gmail.com>
2019-05-11 22:42:38 -0700

    👯‍♀️ Update Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,5 +3,7 @@ import React from 'react';
 import css from '../styles/title.module.css';
 
 export default function Title() {
-  return <h1 className={css.title}>Hi! I'm Sumayyah</h1>;
+  return (
+    <h1 className={css.title}>Hi! I'm Sumayyah. Full-stack web developer. Dessert connoisseur.</h1>
+  );
 }
