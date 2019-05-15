b1d5486c0f04890f40ed00b620358663a5a6edb1
sumi419 <sasgar04@gmail.com>
2019-05-14 20:06:55 -0700

    👩‍💻🍨 Import Projects component to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 import Title from './components/Title';
+import Projects from './components/Projects';
 
 import css from './styles/app.module.css';
 
 export default function App() {
   return (
     <main className={css.container}>
-      <Title />
+      <div>
+        <Title />
+        <Projects />
+      </div>
     </main>
   );
 }
