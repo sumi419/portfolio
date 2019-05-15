b0a8bac1cc42ab36d60ffb8a2d65ece45d64243c
sumi419 <sasgar04@gmail.com>
2019-05-14 20:05:13 -0700

    🧝🏽‍♀️✨ Create Projects.js component

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import css from '../styles/projects.module.css';
+
+export default function Projects() {
+  return (
+    <div>
+      <h2 className={css.title}>Projects</h2>
+    </div>
+  );
+}
