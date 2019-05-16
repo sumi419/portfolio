bd8b7ee9b276b2c00d30521b46abf705bd06209c
sumi419 <sasgar04@gmail.com>
2019-05-15 19:38:59 -0700

    🧝🏽‍♀️ Update Title.js component

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,11 +4,11 @@ import css from '../styles/title.module.css';
 
 export default function Title() {
   return (
-    <div className={css.container}>
+    <section className={css.container}>
       <h1 className={css.title}>
         Hi! I'm Sumayyah. Full-stack web developer. Dessert connoisseur.
       </h1>
       <p className={css.description}>Lambda School graduate</p>
-    </div>
+    </section>
   );
 }
