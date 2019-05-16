c273895ece0bcf40ee071c40182d0cad39358362
sumi419 <sasgar04@gmail.com>
2019-05-15 18:36:03 -0700

    🧜🏽‍♀️✨ Update Projects.js component with styles

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,33 @@ import css from '../styles/projects.module.css';
 
 export default function Projects() {
   return (
-    <div>
+    <div className={css.container}>
       <h2 className={css.title}>Projects</h2>
+      <div className={css.projects}>
+        <div className={css.project}>
+          <p className={css.category}>PERSONAL</p>
+          <h3 className={css.projectTitle}>LearnLocker</h3>
+          <p className={css.description}>
+            A web application designed to keep track of courses and articles in one place and see
+            what other users are learning.
+          </p>
+        </div>
+        <div className={css.project}>
+          <p className={css.category}>PERSONAL</p>
+          <h3 className={css.projectTitle}>Slay All Day</h3>
+          <p className={css.description}>
+            An app where users can search through makeup products across purchasing sites, all in
+            one place.
+          </p>
+        </div>
+        <div className={css.project}>
+          <p className={css.category}>PERSONAL</p>
+          <h3 className={css.projectTitle}>Lambda Notes</h3>
+          <p className={css.description}>
+            An app that allows a user to create, update and delete their notes.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
