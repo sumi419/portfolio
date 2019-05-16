f6176305d7d503ed9255f7bfa8863e25077eabd2
sumi419 <sasgar04@gmail.com>
2019-05-16 15:44:48 -0700

    🧝🏽‍♀️ Add links to project title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,9 @@ export default function Projects() {
       <ul className={css.projects}>
         <li className={css.project}>
           <p className={css.category}>PERSONAL</p>
-          <h3 className={css.projectTitle}>LearnLocker</h3>
+          <a href='https://learnlocker.dev' alt='LearnLocker'>
+            <h3 className={css.projectTitle}>LearnLocker</h3>
+          </a>
           <p className={css.description}>
             A web application designed to keep track of courses and articles in one place and see
             what other users are learning.
@@ -17,7 +19,9 @@ export default function Projects() {
         </li>
         <li className={css.project}>
           <p className={css.category}>PERSONAL</p>
-          <h3 className={css.projectTitle}>Slay All Day</h3>
+          <a href='http://sumi-makeup.netlify.com' alt='Makeup'>
+            <h3 className={css.projectTitle}>Slay All Day</h3>
+          </a>
           <p className={css.description}>
             An app where users can search through makeup products across purchasing sites, all in
             one place.
@@ -25,7 +29,9 @@ export default function Projects() {
         </li>
         <li className={css.project}>
           <p className={css.category}>PERSONAL</p>
-          <h3 className={css.projectTitle}>Lambda Notes</h3>
+          <a href='https://sumi-notes.netlify.com' alt='Lambda Notes'>
+            <h3 className={css.projectTitle}>Lambda Notes</h3>
+          </a>
           <p className={css.description}>
             An app that allows a user to create, update and delete their notes.
           </p>
