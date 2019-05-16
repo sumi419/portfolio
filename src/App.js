70c577287a00e077bdfb5a003eb18e052d3909eb
sumi419 <sasgar04@gmail.com>
2019-05-15 20:13:57 -0700

    🎀 Import Header.js in App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import Title from './components/Title';
 import Projects from './components/Projects';
+import Header from './components/Header';
 
 import css from './styles/app.module.css';
 
 export default function App() {
   return (
-    <main className={css.container}>
-      <Title />
-      <Projects />
-    </main>
+    <>
+      <Header />
+      <main className={css.container}>
+        <Title />
+        <Projects />
+      </main>
+    </>
   );
 }
