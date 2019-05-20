d758af89f666a331edbafdc5052b32e9f5c4402a
sumi419 <sasgar04@gmail.com>
2019-05-20 13:07:14 -0700

    🍋 Add projects data to static directory

---
+++ src/static/lib/projects.js
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    category: 'capstone',
+    title: 'LearnLocker',
+    description:
+      'A web application designed to keep track of courses and articles in one place and see what other users are learning.',
+    view: 'https://learnlocker.dev',
+    source: 'https://github.com/learneda'
+  },
+  {
+    category: 'personal',
+    title: 'Slay All Day',
+    description:
+      'An app where users can search through makeup products across purchasing sites, all in one place.',
+    view: 'http://sumi-makeup.netlify.com',
+    source: 'https://github.com/sumi419/highlight'
+  },
+  {
+    category: 'personal',
+    title: 'Lambda Notes',
+    description: 'An app that allows a user to create, update and delete their notes.',
+    view: 'https://sumi-notes.netlify.com',
+    source: 'https://github.com/sumi419/front-end-project-week'
+  }
+];
