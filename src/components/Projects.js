0d15cf6447af9a3cf7a1ec682d3c71164105f204
sumi419 <sasgar04@gmail.com>
2019-05-24 02:54:00 -0500

    ❤️ Update Projects.js with atomic css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,25 +2,23 @@ import React from 'react';
 
 import { projects } from '../static/lib/projects';
 
-import css from '../styles/projects.module.css';
-
 export default function Projects() {
   return (
-    <section className={css.container}>
-      <h2 className={css.title}>Projects</h2>
-      <ul className={css.projects}>
+    <section className='pt4'>
+      <h2 className='bb bc20 black60 fs16 fs20-td fw500 pb2'>Projects</h2>
+      <ul className='flex-m flex-d flex-row'>
         {projects.map((project) => (
-          <li className={css.project} key={project.title}>
-            <div className={css.category}>{project.category}</div>
+          <li className='bb b-d bc10 pb4 pt4 w30-d' key={project.title}>
+            <div className='black60 fs14 fw500 category'>{project.category}</div>
             <a href={project.view} target='_blank' rel='noopener noreferrer'>
-              <h3 className={css.projectTitle}>{project.title}</h3>
+              <h3 className='fs24 fs36-d fw500 pb1 pt1 hover'>{project.title}</h3>
             </a>
-            <p className={css.description}>{project.description}</p>
+            <p className='black60 fs16 fw500 lh pb1 pt1'>{project.description}</p>
             <a href={project.view} target='_blank' rel='noopener noreferrer'>
-              <span className={css.view}>View</span>
+              <span className='black60 fs14 mr1 hover'>View</span>
             </a>
             <a href={project.source} target='_blank' rel='noopener noreferrer'>
-              <span className={css.source}>Source</span>
+              <span className='black60 fs14 hover'>Source</span>
             </a>
           </li>
         ))}
