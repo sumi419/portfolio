a19b5e0744fb86ceb14d01edeb9c8692d14085bf
sumi419 <sasgar04@gmail.com>
2019-05-24 02:54:47 -0500

    💕 Add links data

---
+++ src/static/lib/links.js
@@ -0,0 +1,6 @@
+export const links = [
+  { name: 'GitHub', href: 'https://github.com/sumi419' },
+  { name: 'LinkedIn', href: 'https://www.linkedin.com/in/sumayyahasgar/' },
+  { name: 'Twitter', href: 'https://twitter.com/sumayyahasgar' },
+  { name: 'Email', href: 'mailto:sasgar04@gmail.com' }
+];
