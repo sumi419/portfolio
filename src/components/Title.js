0d28042adea1214a4e18c41725ecccd611aaebc6
sumi419 <sasgar04@gmail.com>
2019-05-25 14:03:16 -0500

    🧜🏽‍♀️ Use secondary font style on Title's bio

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,7 +6,9 @@ export default function Title() {
       <h1 className='fs24 fs36-t fs48-d pb32'>
         Hi! I'm Sumayyah. Full-stack web developer. Dessert connoisseur.
       </h1>
-      <p className='black60 fs16 fs20-td fw500 lh'>Lambda School graduate</p>
+      <p className='black60 fs16 fs20-td fw500 lh font-style-2'>
+        Benedictine University alumni. Lambda School graduate.
+      </p>
     </section>
   );
 }
