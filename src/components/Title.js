bd3b638c9cddac85b734879f7901b5a99742b657
sumi419 <sasgar04@gmail.com>
2019-05-27 22:42:55 -0500

    🖤 Update Title font color

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
   return (
     <section className='pb4'>
-      <h1 className='fs24 fs36-t fs48-d pb32'>
+      <h1 className='black80 fs24 fs36-t fs48-d pb32'>
         Hi! I'm Sumayyah. Full-stack web developer. Dessert connoisseur.
       </h1>
       <p className='black60 fs16 fs20-td fw500 lh open-sans'>
