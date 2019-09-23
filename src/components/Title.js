e62d0809e1f84fc68644ed00c72f446b209ebe74
sumi419 <sasgar04@gmail.com>
2019-09-23 12:47:47 -0700

    🌺Update Title and background

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,23 +3,24 @@ import React from 'react';
 export default function Title() {
   return (
     <section className='pb4'>
-      <h1 className='fs24 fs36-t fs48-d pb32'>
-        Hi! I'm Sumayyah. Full-stack web developer. Dessert connoisseur.
-      </h1>
+      <h1 className='fs24 fs36-t fs48-d pb32'>Hi! I'm Sumayyah.</h1>
       <p className='black60 fs16 fs20-td lh2 open-sans'>
+        Full-stack web developer. Dessert connoisseur.
         <a
           className='bb pb0_5 hover'
           href='http://www.ben.edu/'
           target='_blank'
           rel='noopener noreferrer'>
-          Benedictine University
+          {' '}
+          Benedictine University{' '}
         </a>{' '}
-        alumni.{' '}
+        alumni.
         <a
           className='bb pb0_5 hover'
           href='https://lambdaschool.com/'
           target='_blank'
           rel='noopener noreferrer'>
+          {' '}
           Lambda School
         </a>{' '}
         graduate.
